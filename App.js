9afc23b2457de0f5f9eb615b027dd521ed8a0a56
MAhmadAzhar <mahmadazhar@outlook.com>
2023-01-18 07:08:26 +0300

    feat: :tada: Basic expense add structure created

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen, HomepageScreen, RegistrationScreen } from './src/screens'
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
+  //States to hold the user status for navigation
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomepageScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Home" component={RegistrationScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
